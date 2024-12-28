808e378ea48b48435111514d3daffd6ab17034ff
Aditya @ArchLinux <132184385+adityadeshlahre@users.noreply.github.com>
2024-12-29 02:15:51 +0530

    ci(deployment): fix docker image build on CI/CD

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({path: __dirname + './../.env'});
 
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 const app = express();
-const port = process.env.API_PORT;
+const port = process.env.API_PORT || 3002;
 
 app.use(cors());
 app.use(bodyParser.json());
--- apps/api/tsup.config.ts
+++ apps/api/tsup.config.ts
@@ -3,6 +3,13 @@ import { defineConfig, type Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entryPoints: ["src/index.ts"],
   clean: true,
+  outDir: "dist",
+  target: "node22",
+  platform: "node",
   format: ["cjs"],
   ...options,
+  external: ['@prisma/client'],
+  copy: [
+    { src: 'node_modules/.prisma/client/*', dest: 'dist/node_modules/.prisma/client' },
+  ],
 }));
